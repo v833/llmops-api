67f4ec4809f0cfe761270e327be5f5afb516e826
v833 <2507301541@qq.com>
2025-04-27 23:11:01 +0800

    feat(api): 添加API工具模块及相关模型和路由
    添加了API工具模块，包括ApiTool和ApiToolProvider模型，并在router.py中预留了API工具相关路由的注册代码。同时，新增了数据库迁移脚本以创建相应的表结构。

--- internal/model/__init__.py
+++ internal/model/__init__.py
@@ -1,5 +1,8 @@
 from .app import App
+from .api_tool import ApiTool, ApiToolProvider
 
 __all__ = [
     "App",
+    "ApiTool",
+    "ApiToolProvider",
 ]
--- internal/router/router.py
+++ internal/router/router.py
@@ -62,5 +62,39 @@ class Router:
             view_func=self.builtin_tool_handler.get_categories,
         )
 
-        # 3.在应用上去注册蓝图
+        # 4.自定义API插件模块
+        # bp.add_url_rule(
+        #     "/api-tools",
+        #     view_func=self.api_tool_handler.get_api_tool_providers_with_page,
+        # )
+        # bp.add_url_rule(
+        #     "/api-tools/validate-openapi-schema",
+        #     methods=["POST"],
+        #     view_func=self.api_tool_handler.validate_openapi_schema,
+        # )
+        # bp.add_url_rule(
+        #     "/api-tools",
+        #     methods=["POST"],
+        #     view_func=self.api_tool_handler.create_api_tool_provider,
+        # )
+        # bp.add_url_rule(
+        #     "/api-tools/<uuid:provider_id>",
+        #     view_func=self.api_tool_handler.get_api_tool_provider,
+        # )
+        # bp.add_url_rule(
+        #     "/api-tools/<uuid:provider_id>",
+        #     methods=["POST"],
+        #     view_func=self.api_tool_handler.update_api_tool_provider,
+        # )
+        # bp.add_url_rule(
+        #     "/api-tools/<uuid:provider_id>/tools/<string:tool_name>",
+        #     view_func=self.api_tool_handler.get_api_tool,
+        # )
+        # bp.add_url_rule(
+        #     "/api-tools/<uuid:provider_id>/delete",
+        #     methods=["POST"],
+        #     view_func=self.api_tool_handler.delete_api_tool_provider,
+        # )
+
+        # 在应用上去注册蓝图
         app.register_blueprint(bp)
