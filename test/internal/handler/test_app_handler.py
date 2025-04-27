d44a38d4b2e53befbcb36e5e4b3335083b1802eb
v833 <2507301541@qq.com>
2025-04-27 22:20:19 +0800

    test: 更新测试用例以匹配新的API路径和参数
    - 删除 `test/conftest.py` 文件，因其内容已不再使用
    - 更新 `test_app_handler.py` 中的测试用例，增加 `app_id` 参数并调整 API 路径
    - 新增 `test_builtin_tool_handlder.py` 文件，添加内置工具处理器的测试用例

--- test/internal/handler/test_app_handler.py
+++ test/internal/handler/test_app_handler.py
@@ -6,9 +6,15 @@ from pkg.response import HttpCode
 class TestAppHandler:
     """app控制器的测试类"""
 
-    @pytest.mark.parametrize("query", [None, "你好，你是谁?"])
-    def test_completion(self, query, client):
-        resp = client.post("/completion", json={"query": query})
+    @pytest.mark.parametrize(
+        "app_id, query",
+        [
+            ("2b2a4e9d-cfc5-4021-bcce-6f098b6c940b", None),
+            ("2b2a4e9d-cfc5-4021-bcce-6f098b6c940b", "你好，你是?"),
+        ],
+    )
+    def test_completion(self, app_id, query, client):
+        resp = client.post(f"/apps/{app_id}/debug", json={"query": query})
         assert resp.status_code == 200
         if query is None:
             assert resp.json.get("code") == HttpCode.VALIDATE_ERROR
