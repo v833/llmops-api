99e1e1c3a33dd1beb64ba57db1bea5608be97166
v833 <2507301541@qq.com>
2025-05-22 23:17:31 +0800

    feat: 优化日志记录器初始化，调整日志级别设置和处理器使用

--- internal/extension/logging_extension.py
+++ internal/extension/logging_extension.py
@@ -1,22 +1,29 @@
 import logging
 import os.path
-from logging.handlers import TimedRotatingFileHandler
 
+from concurrent_log_handler import ConcurrentTimedRotatingFileHandler
 from flask import Flask
 
 
 def logging_init_app(app: Flask):
     """日志记录器初始化"""
-    # 1.设置日志存储的文件夹，如果不存在则创建
+    # 1.根据不同的环境设置logging根处理器的日志级别
+    logging.getLogger().setLevel(
+        logging.DEBUG
+        if app.debug or os.getenv("FLASK_ENV") == "development"
+        else logging.WARNING
+    )
+
+    # 2.设置日志存储的文件夹，如果不存在则创建
     log_folder = os.path.join(os.getcwd(), "storage", "log")
     if not os.path.exists(log_folder):
         os.makedirs(log_folder)
 
-    # 2.定义日志的文件名
+    # 3.定义日志的文件名
     log_file = os.path.join(log_folder, "app.log")
 
-    # 3.设置日志的格式，并且让日志每天更新一次
-    handler = TimedRotatingFileHandler(
+    # 4.设置日志的格式，并且让日志每天更新一次
+    handler = ConcurrentTimedRotatingFileHandler(
         log_file,
         when="midnight",
         interval=1,
@@ -26,11 +33,15 @@ def logging_init_app(app: Flask):
     formatter = logging.Formatter(
         "[%(asctime)s.%(msecs)03d] %(filename)s -> %(funcName)s line:%(lineno)d [%(levelname)s]: %(message)s"
     )
-    handler.setLevel(logging.DEBUG)
+    handler.setLevel(
+        logging.DEBUG
+        if app.debug or os.getenv("FLASK_ENV") == "development"
+        else logging.WARNING
+    )
     handler.setFormatter(formatter)
     logging.getLogger().addHandler(handler)
 
-    # 4.在开发环境下同时将日志输出到控制台
+    # 5.在开发环境下同时将日志输出到控制台
     if app.debug or os.getenv("FLASK_ENV") == "development":
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(formatter)
